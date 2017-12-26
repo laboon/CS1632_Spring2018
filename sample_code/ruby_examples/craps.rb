0d89265258e4a850a61ceff6555f32004c4a09f6
Bill Laboon <laboon@laboon.laboon>
2017-12-26 06:31:08 -0800

    craps working, need to comment

--- sample_code/ruby_examples/craps.rb
+++ sample_code/ruby_examples/craps.rb
@@ -1,7 +1,17 @@
 # craps.rb
 
+# Create a class Die, with one method, roll, which will return a value
+# from the range of 1..6
+
 class Die
 
+  # Turn the Range object (1..6) to an array with .to_a
+  # This will allow us to use the Enumerable methods such as
+  # .sample, which gives us a random element selected from
+  # the POSSIBLE_VALUES array
+
+  # Another way to do this would be rand(6) + 1
+  
   POSSIBLE_VALUES = (1..6).to_a
 
   def roll
@@ -10,26 +20,14 @@ class Die
   
 end
 
-COME_OUT_LOSS = [2, 3, 12]
-COME_OUT_WIN = [7, 11]
-
-def won_come_out val
-  COME_OUT_WIN.include?(val)
-end
-
-def lose_come_out
-  COME_OUT_LOSE.include?(val)
-end
-
-
-def won_other_rounds val, orig
-  val == orig
-end
-
 def print_line
+  # Print the '*' character 20 times to make a line
   20.times { print '*' }
+  # Now add a carriage return
   puts
 end
+
+
 
 def print_roll d1_val, d2_val
   val = d1_val + d2_val
@@ -38,6 +36,7 @@ def print_roll d1_val, d2_val
   puts "Die 2 roll: #{d2_val}"
   puts "Total     : #{val}"
   print_line
+  val
 end
 
 def roll
@@ -51,19 +50,20 @@ end
 
 # EXECUTION STARTS HERE
 
-# puts "Enter your name > "
-# name = gets.chomp
+puts "Enter your name > "
+name = gets.chomp
 
-game_over = false
 game_status = :not_over
 come_out = true
 point = nil
 
-until (game_over) do
+until (game_status != :not_over) do
   
   d1_val, d2_val = roll
   val = print_roll d1_val, d2_val
+  
   if come_out
+    come_out = false
     case val
     when 2, 3, 12
       game_status = :lose
@@ -73,8 +73,19 @@ until (game_over) do
       point = val
     end
   else
+    if val == 7
+      game_status = :lose
+    elsif val == point
+      game_status = :win
+    else
+      # Do nothing, game is still in play
+    end
+  end
     
-          
-  game_over = true
 end
 
+if game_status == :win
+  puts "You win, #{name}!"
+else
+  puts "You lose, #{name}!"
+end
