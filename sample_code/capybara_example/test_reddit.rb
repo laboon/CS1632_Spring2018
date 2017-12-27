66b08eea5081c400dcadf4f65560cfcc26317ca0
Bill Laboon <laboon@laboon.laboon>
2017-12-26 16:41:40 -0800

    start capybara

---
+++ sample_code/capybara_example/test_reddit.rb
@@ -0,0 +1,11 @@
+class CapybaraTestCase < Minitest::Test
+  include Capybara::DSL
+  include Capybara::Minitest::Assertions
+
+  def teardown
+    Capybara.reset_sessions!
+    Capybara.use_default_driver
+  end
+end
+
+# Capybara.javascript_driver = :webkit
