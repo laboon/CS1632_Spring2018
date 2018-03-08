7a94ceb28cd1dd1966e6bcd0c950a291011e13a7
Bill Laboon <laboon@laboon.laboon>
2018-03-08 14:30:14 -0500

    fixes #27

--- sample_code/mock_example/graph_test.rb
+++ sample_code/mock_example/graph_test.rb
@@ -99,7 +99,7 @@ class GraphTest < Minitest::Test
     # graph.add_node, this will fail
     mocked_node = Minitest::Mock.new("mocked node")
     # Expect id to be called and make a stub
-    # which just returns new, similar to:
+    # which just returns true, similar to:
     # def mocked_node.id; true; end
     mocked_node.expect :id, true
     mocked_node.expect :id, true
